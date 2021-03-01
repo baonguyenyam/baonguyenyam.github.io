9185cdfdd305fc7522d73285354d2b22ab3e0688
Nguyen Pham <baonguyenyam@gmail.com>
2021-02-28 18:45:22 -0600

    update

--- demo/lift/js/main.js
+++ demo/lift/js/main.js
@@ -85,4 +85,44 @@ $('.anim-text-flow').html(function (i, html) {
   var chars = $.trim(html).split("");
   return '<span>' + chars.join('</span><span>') + '</span>';
 });
+
+function setHeader(elm) {
+  var mmn;
+
+  if ($(window).width <= 768) {
+    mmn = 70;
+  } else {
+    mmn = 120;
+  }
+
+  if (elm >= mmn) {
+    $("header").addClass('active');
+  } else {
+    $("header").removeClass('active');
+  }
+}
+
+$(document).ready(function () {
+  var mmn;
+
+  if ($(window).width <= 768) {
+    mmn = 70;
+  } else {
+    mmn = 120;
+  }
+
+  $("header").addClass('fixedheader');
+
+  if ($(window).scrollTop() >= mmn) {
+    setHeader($(window).scrollTop());
+  }
+
+  if ($("header").hasClass("fixedheader")) {
+    $("main").addClass('main-fixedheader');
+  }
+}); // Fixed Header
+
+$(window).scroll(function () {
+  setHeader($(document).scrollTop());
+});
 //# sourceMappingURL=main.js.map
