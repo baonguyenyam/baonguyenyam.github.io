564d62c715bd201d9fb045d4c219868e0554e336
baonguyenyam <baonguyenyam@gmail.com>
2021-03-14 01:19:49 -0600

    done

--- demo/lift/js/main.js
+++ demo/lift/js/main.js
@@ -44,18 +44,123 @@ $(function () {
   if (isAndroid) {
     $('select.form-control').removeClass('form-control').css('width', '100%');
   }
+});
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 20,
+      density: {
+        enable: true,
+        value_area: 200
+      }
+    },
+    color: {
+      value: ["#fff", "#fff", "#fff", "#fff"]
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#fff"
+      },
+      polygon: {
+        nb_sides: 15
+      }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1.5,
+        opacity_min: 0.15,
+        sync: false
+      }
+    },
+    size: {
+      value: 2.5,
+      random: false,
+      anim: {
+        enable: true,
+        speed: 2,
+        size_min: 0.15,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 110,
+      color: "#fff",
+      opacity: 0.25,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 1.6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "repulse"
+      },
+      onclick: {
+        enable: false,
+        mode: "push"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true
 }); // Main
 
 $(document).ready(function () {
   // Phần Slider
-  $('.home-slider .owl-carousel').owlCarousel({
+  $(".home-slider .owl-carousel").owlCarousel({
     items: 1,
     nav: false,
     dots: true,
     navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
   }); // Phần Clients
 
-  $('.home-clients .owl-carousel').owlCarousel({
+  $(".home-clients .owl-carousel").owlCarousel({
     items: 2,
     nav: true,
     dots: false,
@@ -76,11 +181,12 @@ $(document).ready(function () {
     }
   }); // Phần testimonial
 
-  $('.section-testimonial .owl-carousel').owlCarousel({
+  $(".section-testimonial .owl-carousel").owlCarousel({
     items: 1,
     nav: false,
     autoplay: true,
     dots: true,
+    loop: true,
     navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
     responsive: {
       // breakpoint from 480 up
@@ -98,14 +204,14 @@ $(document).ready(function () {
     }
   });
 });
-document.querySelectorAll('.menu').forEach(function (btn) {
-  btn.addEventListener('click', function (e) {
-    btn.classList.toggle('active');
+document.querySelectorAll(".menu").forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    btn.classList.toggle("active");
   });
 });
-$('.anim-text-flow').html(function (i, html) {
+$(".anim-text-flow").html(function (i, html) {
   var chars = $.trim(html).split("");
-  return '<span>' + chars.join('</span><span>') + '</span>';
+  return "<span>" + chars.join("</span><span>") + "</span>";
 });
 
 function setHeader(elm) {
@@ -118,9 +224,9 @@ function setHeader(elm) {
   }
 
   if (elm >= mmn) {
-    $("header").addClass('active');
+    $("header").addClass("active");
   } else {
-    $("header").removeClass('active');
+    $("header").removeClass("active");
   }
 }
 
@@ -133,18 +239,71 @@ $(document).ready(function () {
     mmn = 120;
   }
 
-  $("header").addClass('fixedheader');
+  $("header").addClass("fixedheader");
 
   if ($(window).scrollTop() >= mmn) {
     setHeader($(window).scrollTop());
   }
 
   if ($("header").hasClass("fixedheader")) {
-    $("main").addClass('main-fixedheader');
+    $("main").addClass("main-fixedheader");
   }
 }); // Fixed Header
 
 $(window).scroll(function () {
   setHeader($(document).scrollTop());
+}); // Select all links with hashes
+
+$('a[href*="#"]') // Remove links that don't actually link to anything
+.not('[href="#"]').not('[href="#0"]').click(function (event) {
+  // On-page links
+  if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]"); // Does a scroll target exist?
+
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $("html, body").animate({
+        scrollTop: target.offset().top
+      }, 1000, function () {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+
+        if ($target.is(":focus")) {
+          // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+
+          $target.focus(); // Set focus again
+        }
+      });
+    }
+  }
+});
+var typed = new Typed(".typed span", {
+  // Waits 1000ms after typing "First"
+  stringsElement: "#typed-strings",
+  typeSpeed: 140,
+  backSpeed: 50,
+  smartBackspace: true,
+  // backDelay: 500,
+  // startDelay: 2000,
+  loop: true
+});
+$(function () {
+  setInterval(function () {
+    var $next = $('.tricker em.active').removeClass('active').next('em');
+
+    if ($next.length) {
+      $next.addClass('active');
+    } else {
+      $("em:first").addClass('active');
+    }
+  }, 5000);
 });
 //# sourceMappingURL=main.js.map
