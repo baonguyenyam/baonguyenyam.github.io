01141ae5a92e02cb5391181ff93ec68617d11de8
Nguyen Pham <baonguyenyam@gmail.com>
2021-06-24 12:28:33 -0500

    update chat

--- tasks/concat-css.js
+++ tasks/concat-css.js
@@ -2,7 +2,7 @@ module.exports = function (gulp, $, browserSync) {
 	gulp.task('concat-css', function () {
 		return gulp.src([
 				'bower_components/font-awesome-5/css/all.min.css',
-				// 'bower_components/font-awesome/css/font-awesome.min.css',
+				'src/css/chat.css',
 				'bower_components/animate.css/animate.min.css',
 				// OWL
 				'bower_components/owl.carousel/dist/assets/owl.carousel.min.css',
