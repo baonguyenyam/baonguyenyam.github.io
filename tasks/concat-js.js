82433d0a36d071e1ac704a3f115bf0a05b0abb4b
Nguyen Pham <baonguyenyam@gmail.com>
2021-06-25 09:17:54 -0500

    Fixed bug

--- tasks/concat-js.js
+++ tasks/concat-js.js
@@ -6,10 +6,10 @@ module.exports = function (gulp, $, browserSync) {
 				'bower_components/popper.js/dist/umd/popper.min.js',
 				'bower_components/bootstrap/dist/js/bootstrap.min.js',
 				// Phần Plugins
-				'bower_components/owl.carousel/dist/owl.carousel.min.js',
-				'bower_components/fancybox/dist/jquery.fancybox.min.js',
+				// 'bower_components/owl.carousel/dist/owl.carousel.min.js',
+				// 'bower_components/fancybox/dist/jquery.fancybox.min.js',
 				'bower_components/particles.js/particles.min.js',
-				'bower_components/typed.js/lib/typed.min.js',
+				// 'bower_components/typed.js/lib/typed.min.js',
 			])
 			.pipe($.concat('nguyen.js'))
 			.pipe(gulp.dest('./docs/js'));
